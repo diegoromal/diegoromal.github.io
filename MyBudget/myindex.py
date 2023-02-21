1a1a1b4abed0bc6a809c586324a9079f2aaac78b
diegoromal <diego@qosit.com.br>
2023-02-21 20:29:37 -0300

    MyBudget Initial

--- MyBudget/myindex.py
+++ MyBudget/myindex.py
@@ -6,7 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
-
+from components import sidebar, dashboards, extratos
 
 
 
@@ -15,14 +15,24 @@ content = html.Div(id="page-content")
 
 
 app.layout = dbc.Container(children=[
-
-
-
-
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id='url'),
+            sidebar.layout
+        ], md=2, style={'background-color': 'red', 'height': '1080px'}),
+        dbc.Col([
+            content
+        ], md=10, style={'background-color': 'blue', 'height': '1080px'})
+    ])
 
 ], fluid=True,)
 
-
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def render_page(pathname):
+    if pathname == '/' or pathname == '/dashboards':
+        return dashboards.layout
+    if pathname == '/extratos':
+        return extratos.layout
 
 
 if __name__ == '__main__':
